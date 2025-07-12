84b1a38129fdc1d03f47d51d42fe87a73087c110
habibuoy <habibiemuhammad90@gmail.com>
2025-07-12 11:48:15 +0700

    Chore: Update server configuration code to use the dotenv

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
