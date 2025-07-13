01c6d02068c21c4568bdbaab08dea69211576dbb
habibuoy <habibiemuhammad90@gmail.com>
2025-07-13 23:16:05 +0700

    Fix: Authentication credentials not set correctly

--- src/server.js
+++ src/server.js
@@ -41,7 +41,7 @@ const init = async () => {
     validate: (artifacts) => ({
       isValid: true,
       credentials: {
-        id: artifacts.decoded.payload.id,
+        id: artifacts.decoded.payload.userId,
       },
     }),
   });
